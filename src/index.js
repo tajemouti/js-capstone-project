7162183a17e35fc2bf5d731e3e8349aa291690af
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-10 01:01:42 +0500

    Commnets popup added with API integration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import './style.css';
-import './logo.jpg';
-import { fetchCategoryMeals, displayMeals } from './modules/render.js';
+import "./style.css";
+import "./logo.jpg";
+import { fetchCategoryMeals, displayMeals } from "./modules/render.js";
 
-const headerLinks = document.querySelectorAll('header a');
+const headerLinks = document.querySelectorAll("header a");
 const renderCategory = async (category) => {
   const categoryMeals = await fetchCategoryMeals(category);
   displayMeals(categoryMeals);
 };
 
 headerLinks.forEach((link) => {
-  link.addEventListener('click', async (e) => {
+  link.addEventListener("click", async (e) => {
     e.preventDefault();
     const category = e.target.textContent;
     await renderCategory(category);
   });
 });
 
-renderCategory('Seafood');
+renderCategory("Seafood");
