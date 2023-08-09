e7fd2459f9b9da88175fc1dcade52f11c416391a
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-10 01:03:25 +0500

    fixed some linters

--- src/modules/popupModal.js
+++ src/modules/popupModal.js
@@ -1,9 +1,8 @@
-import getComments from "./getComments";
-import getDetail from "./getDetail";
+import getDetail from './getDetail';
 
-const modal = document.getElementById("modal");
-const closebtn = document.getElementById("close");
-const itemSection = document.getElementById("itemSection");
+const modal = document.getElementById('modal');
+const closebtn = document.getElementById('close');
+const itemSection = document.getElementById('itemSection');
 
 const popupModal = async (id) => {
   const data = await getDetail(`lookup.php?i=${id}`);
@@ -12,31 +11,30 @@ const popupModal = async (id) => {
 };
 
 const displayItemDetail = (data) => {
-  console.log(data);
-  modal.style.display = "flex";
+  modal.style.display = 'flex';
 
   itemSection.innerHTML = `
-    <img class="img" src="${data.strMealThumb}">
+    <img class='img' src='${data.strMealThumb}'>
     <h2>${data.strMeal}</h2>
-    <p class="p">${data.strInstructions}</p>
-    <div class="addComment">
-        <h3 class="commentTitle">Add Comment</h3>
-        <input class="usernameInput" placeholder="Name"/>
-        <textarea id="txtid" name="txtname" rows="4" cols="50" maxlength="200" class="textarea">Add comment here...</textarea>
-        <button id="btnCommentSubmit" class="commentsBtn">Add Comment</button>
+    <p class='p'>${data.strInstructions}</p>
+    <div class='addComment'>
+        <h3 class='commentTitle'>Add Comment</h3>
+        <input class='usernameInput' placeholder='Name'/>
+        <textarea id='txtid' name='txtname' rows='4' cols='50' maxlength='200' class='textarea'>Add comment here...</textarea>
+        <button id='btnCommentSubmit' class='commentsBtn'>Add Comment</button>
     </div>
     `;
 
-  closebtn.addEventListener("click", closeModal);
+  closebtn.addEventListener('click', closeModal);
 };
 
 const closeModal = () => {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 };
 
 window.onclick = (event) => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 };
 
