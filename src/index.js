25d70d3e2ed813cb28ebc319d06d5bda6c6ec81c
tajemouti <tajemouti@gmail.com>
2023-08-09 13:35:46 +0100

    Display items on the homepage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './style.css';
 import './logo.jpg';
+import { fetchSeafoodMeals, displayMeals } from './modules/render.js';
+
+const renderAPI = async () => {
+  const seafoodMeals = await fetchSeafoodMeals();
+  displayMeals(seafoodMeals);
+};
+
+renderAPI();
